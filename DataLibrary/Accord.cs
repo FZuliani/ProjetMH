2130bdba911df629afd78085b0e62b329eed35ad
Faisto <45098661+Faisto@users.noreply.github.com>
2019-04-16 16:31:40 +0200

    Add files via upload

---
+++ DataLibrary/Accord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataLibrary
+{
+    public class Accord
+    {
+
+        private float _Frequency;
+        private string _Accord;
+
+        public string accord
+        {
+            get { return _Accord; }
+            set { _Accord = value; }
+        }
+
+
+        public float Frequency
+        {
+            get { return _Frequency; }
+            set { _Frequency = value; }
+        }
+
+
+        
+    }
+}
