955ab77dd899faf252541f0fe24ce2539b4da0e1
Faisto <45098661+Faisto@users.noreply.github.com>
2019-04-16 17:02:38 +0200

    Add files via upload

---
+++ ToolBoxMVVM/Commands/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ToolBoxMVVM.Commands
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+
+        private readonly Action _Methode;
+        private readonly Func<bool> _canExecuteMethode;
+
+        public Command(Action methode, Func<bool> canExecuteMethode = null)
+        {
+            _Methode = methode;
+            _canExecuteMethode = canExecuteMethode;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteMethode == null)
+            {
+                return true;
+            }
+            return _canExecuteMethode.Invoke();
+        }
+        
+
+        public void Execute(object parameter)
+        {
+
+            _Methode?.Invoke();
+
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, null);
+        }
+    }
+}
