bcc41dc4c7c03ab071e0a95d28ad05f8dafd3762
Faisto <45098661+Faisto@users.noreply.github.com>
2019-04-16 16:42:44 +0200

    Add files via upload

---
+++ InterfaceMH/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using InterfaceMH.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InterfaceMH
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+        public static ServiceLocator serviceLocator { get; set; }
+
+        public App()
+        {
+            serviceLocator = new ServiceLocator();
+        }
+
+    }
+}
